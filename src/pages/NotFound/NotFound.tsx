ad3def31e9b3fa9a41bc88ab46d0265cb1296fb5
labibahmed10 <labib.ahmed.372@gmail.com>
2024-09-17 10:02:24 +0600

    feat: added the not found page with homepage navigate

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,21 @@
 import { Button, Result } from "antd";
+import { NavLink } from "react-router-dom";
 
 export default function NotFound() {
-  return <Result status="404" title="404" subTitle="Sorry, the page you visited does not exist." extra={<Button type="primary" className="bg-[#]">Back Home</Button>} />;
+  return (
+    <section className="grid place-content-center h-screen">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <NavLink to="/">
+            <Button type="primary" className="bg-[#223E51]">
+              Back Home
+            </Button>
+          </NavLink>
+        }
+      />
+    </section>
+  );
 }
